c702361879109230fe663d764335de932ddcfd10
9551Dev <oliverekcaha@gmail.com>
2022-09-16 01:55:42 +0200

    added a check for existing love.run

--- core/default_run.lua
+++ core/default_run.lua
@@ -1,29 +1,31 @@
 local function build_run(love,args)
-    function love.run()
-        if love.load then love.load(table.unpack(args,1,args.n)) end
-        if love.timer then love.timer.step() end
-        local dt = 0
-        return function()
-            if love.event then
-                love.event.pump()
-                for name, a,b,c,d,e,f in love.event.poll() do
-                    if name == "quit" then
-                        if not love.quit or not love.quit() then
-                            return a or 0
+    if not love.run then
+        function love.run()
+            if love.load then love.load(table.unpack(args,1,args.n)) end
+            if love.timer then love.timer.step() end
+            local dt = 0
+            return function()
+                if love.event then
+                    love.event.pump()
+                    for name, a,b,c,d,e,f in love.event.poll() do
+                        if name == "quit" then
+                            if not love.quit or not love.quit() then
+                                return a or 0
+                            end
                         end
+                        love.handlers[name](a,b,c,d,e,f)
                     end
-                    love.handlers[name](a,b,c,d,e,f)
                 end
+                if love.timer then dt = love.timer.step() end
+                if love.update then love.update(dt) end
+                if love.graphics and love.graphics.isActive() then
+                    love.graphics.origin()
+                    love.graphics.clear(love.graphics.getBackgroundColor())
+                    if love.draw then love.draw() end
+                    love.graphics.present()
+                end
+                if love.timer then love.timer.sleep(0.001) end
             end
-            if love.timer then dt = love.timer.step() end
-            if love.update then love.update(dt) end
-            if love.graphics and love.graphics.isActive() then
-                love.graphics.origin()
-                love.graphics.clear(love.graphics.getBackgroundColor())
-                if love.draw then love.draw() end
-                love.graphics.present()
-            end
-            if love.timer then love.timer.sleep(0.001) end
         end
     end
 end
